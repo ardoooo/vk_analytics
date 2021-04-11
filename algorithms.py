82f885eafa4f808856c85466468c56ac15782dc6
ardoooo <naumov.av@phystech.edu>
2021-04-11 13:11:23 +0300

    update 1.1

--- algorithms.py
+++ algorithms.py
@@ -1,8 +1,6 @@
 import vk_api
-import datetime
-
-
-token = "9f8bfbdd4759757d4c366a7e5a37b7daba1b436861f96587c2caa082b84221ed9a60c1afc5843c189f0d2"
+from datetime import datetime
+from config import token
 
 '''класс TableOfPosts, хранящий все посты по стены конкретного сообщества,
 стастику о сообществе мы получаем с помощью методо класса'''
@@ -10,80 +8,34 @@ token = "9f8bfbdd4759757d4c366a7e5a37b7daba1b436861f96587c2caa082b84221ed9a60c1a
 
 class TableOfPosts:
 
+    parameters = ['likes', 'reposts', 'comments', 'views']
+
     def __init__(self, domain, start_date, end_date):
         self.domain = domain
-        self.start_date = datetime.datetime.strptime(str(start_date), '%d.%m.%y')
-        self.end_date = datetime.datetime.strptime(str(end_date), '%d.%m.%y')
-        self.start_date = self.start_date.timestamp()
-        self.end_date = self.end_date.timestamp()
+        self.start_date = datetime.strptime(str(start_date), '%d.%m.%y').timestamp()
+        self.end_date = datetime.strptime(str(end_date), '%d.%m.%y').timestamp()
 
         vk_session = vk_api.VkApi(token=token)
         tools = vk_api.VkTools(vk_session)
         self.wall = tools.get_all('wall.get', 100, {'domain': self.domain})
 
         # при инициализации класса сразу насчитаем некоторые параметры, для ускорения работы приложения
-        self.common_count_likes = 0
-        self.common_count_reposts = 0
-        self.common_count_views = 0
-        self.common_count_comments = 0
-        self.max_count_likes = 0
-        self.max_count_reposts = 0
-        self.max_count_views = 0
-        self.max_count_comments = 0
+        self.common_value = dict.fromkeys(TableOfPosts.parameters, 0)
+        self.max_value = dict.fromkeys(TableOfPosts.parameters, 0)
+        self.count_posts = 0
+
         for post in self.wall['items']:
             if post['date'] <= self.end_date:
                 if post['date'] >= self.start_date:
-                    self.common_count_likes += post['likes']['count']
-                    self.max_count_likes = max(self.max_count_likes, post['likes']['count'])
-
-                    self.common_count_reposts += post['reposts']['count']
-                    self.max_count_reposts = max(self.max_count_reposts, post['reposts']['count'])
-
-                    self.common_count_views += post['views']['count']
-                    self.max_count_views = max(self.max_count_views, post['views']['count'])
-
-                    self.common_count_comments += post['comments']['count']
-                    self.max_count_comments = max(self.max_count_comments, post['comments']['count'])
+                    for param in TableOfPosts.parameters:
+                        self.common_value[param] += post[param]['count']
+                        self.max_value[param] = max(post[param]['count'], self.max_value[param])
+                    self.count_posts += 1
                 else:
                     break
 
-    # метод, возвращающий среднее количество лайков
-    def likes_average(self):
-        if self.wall['count'] == 0:
-            return 0
-        return self.common_count_likes / self.wall['count']
-
-    # метод, возвращающий среднее количество лайков
-    def reposts_average(self):
-        if self.wall['count'] == 0:
-            return 0
-        return self.common_count_reposts / self.wall['count']
-
-    # метод, возвращающий среднее количество комментариев
-    def comments_average(self):
-        if self.wall['count'] == 0:
-            return 0
-        return self.common_count_comments / self.wall['count']
-
-    # метод, возвращающий среднее количество просмотров
-    def views_average(self):
-        if self.wall['count'] == 0:
-            return 0
-        return self.common_count_views / self.wall['count']
-
-
-# метод, возвращающий максимальное количество лайков
-    def likes_max(self):
-        return self.max_count_likes
-
-    # метод, возвращающий максимальное количество лайков
-    def reposts_max(self):
-        return self.max_count_reposts
-
-    # метод, возвращающий максимальное количество комментариев
-    def comments_max(self):
-        return self.max_count_comments
-
-    # метод, возвращающий максимальное количество просмотров
-    def views_max(self):
-        return self.max_count_views
+    def __getattr__(self, attr_name):
+        if attr_name[-7:] == 'average':
+            return self.common_value[attr_name[:-8]] / self.count_posts
+        if attr_name[-3:] == 'max':
+            return self.max_value[attr_name[:-4]]
